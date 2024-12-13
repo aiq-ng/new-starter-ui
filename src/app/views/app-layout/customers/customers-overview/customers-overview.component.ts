734cee93733675e039ec8c415a9d2950010399c3
iamemmanuelakinola <emmanuelakinola255@gmail.com>
2024-12-13 11:55:14 +0100

    customer-overview

--- src/app/views/app-layout/customers/customers-overview/customers-overview.component.ts
+++ src/app/views/app-layout/customers/customers-overview/customers-overview.component.ts
@@ -7,4 +7,78 @@ import { Component } from '@angular/core';
 })
 export class CustomersOverviewComponent {
 
+  ngOnInit() {
+    this.selectedCustomer = this.customers[0]; // Default selection
+  }
+
+  selectCustomer(customer: any) {
+    this.selectedCustomer = customer;
+  }
+
+  customers = [
+    {
+      name: 'Mr. Maxwell',
+      email: 'maxwell@example.com',
+      phone: '+2348012345678',
+      amount: '₦1,500,000',
+      status: 'Owing',
+      type: 'Individual',
+      currency: 'NGN',
+      paymentTerms: 'Net 30',
+      transactions: [
+        {
+          date: '02 Nov 2024 03:35 PM',
+          description: 'Invoice Added',
+          details: 'Invoice INV-000001 emailed by Susan Daniel',
+          currency: 'NGN',
+          receivables: '₦500,000',
+          paid: '₦300,000'
+        },
+        {
+          date: '31 Nov 2024 03:35 PM',
+          description: 'Invoice Added',
+          details: 'Invoice INV-000001 emailed by Susan Daniel',
+          currency: 'NGN',
+          receivables: '₦400,000',
+          paid: '₦200,000'
+        },
+      ],
+    },
+    {
+      name: 'Agro Tech',
+      email: 'agrotech@example.com',
+      phone: '+2347032345678',
+      amount: '₦2,000,000',
+      status: 'Paid',
+      type: 'Business',
+      currency: 'NGN',
+      paymentTerms: 'Net 60',
+      transactions: [],
+    },
+    {
+      name: 'Maxwell Book Launch',
+      email: 'agrotech@example.com',
+      phone: '+2347032345678',
+      amount: '₦1,500,000',
+      status: 'Due in 5 days',
+      type: 'Business',
+      currency: 'NGN',
+      paymentTerms: 'Net 60',
+      transactions: [],
+    },
+      {
+      name: 'Jules Book Launch',
+      email: 'agrotech@example.com',
+      phone: '+2347032345678',
+      amount: '₦2,000,000',
+      status: 'Owing',
+      type: 'Business',
+      currency: 'NGN',
+      paymentTerms: 'Net 60',
+      transactions: [],
+    },
+  ];
+
+  selectedCustomer: any;
+
 }
