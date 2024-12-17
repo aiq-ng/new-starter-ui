f85318fc4d971017224694a7d93d854987642f6c
wobin1 <nathanielmusa@gmail.com>
2024-12-17 10:41:34 +0100

    some changes

--- src/app/views/app-layout/purchases/create-new-purchase-order/create-new-purchase-order.component.ts
+++ src/app/views/app-layout/purchases/create-new-purchase-order/create-new-purchase-order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-new-purchase-order',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './create-new-purchase-order.component.scss'
 })
 export class CreateNewPurchaseOrderComponent {
+  purchaseOrderForm:any;
+  isSubmitted:boolean = false;
+  loading:boolean = false;
+  vendors:any;
+  branches:any;
+  paymentTerms:any;
+
+
+
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(){
+    this.purchaseOrderForm = this.fb.group({
+      vendor_id: ['', Validators.required],
+      branch_id: ['', Validators.required],
+      delivery_date: ['', Validators.required],
+      payment_term_id: ['', Validators.required],
+      subject: ['', Validators.required],
+      notes: ['', Validators.required],
+      terms_and_conditions: ['', Validators.required],
+      discount: ['', Validators.required],
+      shipping_charge: ['', Validators.required],
+      total: ['', Validators.required]
+    });
+  }
+
+  get f(){return this.purchaseOrderForm.controls}
+
+  savePurchase(){}
 
 }
