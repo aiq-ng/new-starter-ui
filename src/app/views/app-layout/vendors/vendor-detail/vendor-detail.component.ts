28131cd1f1c9c344cf1847e9317070542a25333d
wobin1 <nathanielmusa@gmail.com>
2024-12-17 16:32:59 +0100

    added vendor detail

--- src/app/views/app-layout/vendors/vendor-detail/vendor-detail.component.ts
+++ src/app/views/app-layout/vendors/vendor-detail/vendor-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpServiceService } from '../../../../services/http-service.service';
 
 @Component({
   selector: 'app-vendor-detail',
@@ -9,14 +10,44 @@ import { Router } from '@angular/router';
 export class VendorDetailComponent {
   vendorName:any;
   tabMenu= ['Overview', 'transactions']
-  constructor(private router:Router){}
+  item_id:any;
+  pageLoading:any;
+  vendorDetail:any;
 
+
+  constructor(private router:Router, private api:HttpServiceService){}
   ngOnInit(){
-    this.vendorName = 'Agro Tech';
+    this.getItemDetail()
   }
 
+  getParamsId(){
+    const url = window.location.href;
+    console.log('url', url);
+    const segments = url.split('/');
+    this.item_id = segments[segments.length - 1];
+
+    return this.item_id;
+  }
+
+  getItemDetail(){
+    this.pageLoading= true;
+    return this.api.get('vendors/' + this.getParamsId()).subscribe(
+      res =>{
+        let response:any = res
+        this.vendorDetail = response.data
+        console.log(this.vendorDetail)
+        this.pageLoading=false;
+      }, err=>{
+        console.log(err)
+        this.pageLoading=false;
+      }
+    )
+
+  }
   route(page:any){
     this.router.navigate([page]);
   }
+
+
 
 }
