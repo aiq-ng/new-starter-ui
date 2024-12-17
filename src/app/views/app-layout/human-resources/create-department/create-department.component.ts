8316a27dc6d8e4d8c781ef0e3eb550aaf51a2c39
wobin1 <nathanielmusa@gmail.com>
2024-12-17 18:30:21 +0100

    working on create department

--- src/app/views/app-layout/human-resources/create-department/create-department.component.ts
+++ src/app/views/app-layout/human-resources/create-department/create-department.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpServiceService } from '../../../../services/http-service.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-create-department',
@@ -8,15 +10,85 @@ import { FormBuilder } from '@angular/forms';
 })
 export class CreateDepartmentComponent {
   createDepartmentForm:any;
+  saveDeparment:any;
+  pageLoading:any;
+  positions:any;
+  baseSalaryType:any;
+  isSubmitted:boolean = false;
 
-  constructor(private fb:FormBuilder){}
+  constructor(private fb:FormBuilder, private api:HttpServiceService, private messageService: MessageService){}
 
 
   ngOnInit(){
+    this.createDepartmentForm = this.fb.group({
+      name: ['', Validators.required],
+      salary_type: ['', Validators.required],
+      base_type_id: [''],
+      base_salary: [''],
+      description: [''],
+    });
+
+    this.getPositions();
+    this.getBaseSalaryType()
   }
 
+  get f(){return this.createDepartmentForm.controls}
+
   createDepartment(){
-    // TODO: Implement logic to create a new department
+    this.isSubmitted = true;
+    if(this.createDepartmentForm.invalid){
+    console.log(this.createDepartmentForm.value);
+    console.log('Invalid form')
+      return;
+    }
+
+    console.log(this.createDepartmentForm.value);
+    this.api.post('human-resources/departments', this.createDepartmentForm.value).subscribe(
+      res=>{
+        console.log(res)
+        this.showSuccess('Department created successfully');
+        this.createDepartmentForm.reset();
+      },
+      err=>{
+        console.log(err)
+        this.showError('Failed to create department');
+      }
+    )
+  }
+
+  getPositions(){
+    this.pageLoading = true;
+    this.api.get('roles').subscribe(
+      res=>{
+        this.positions = res;
+        this.pageLoading = false;
+      },
+      err=>{
+        console.log(err)
+      }
+    )
+  }
+
+  getBaseSalaryType(){
+    this.pageLoading = true;
+    this.api.get('base-pay-types').subscribe(
+      res=>{
+        this.baseSalaryType = res;
+        this.pageLoading = false;
+      },
+      err=>{
+        console.log(err)
+      }
+    )
+  }
+
+  showSuccess(message: string) {
+    console.log('showSuccess')
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: message });
+  }
+
+  showError(message: string) {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: message });
   }
 
 }
--- src/app/views/app-layout/human-resources/staff-profile/staff-profile.component.ts
+++ src/app/views/app-layout/human-resources/staff-profile/staff-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpServiceService } from '../../../../services/http-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-staff-profile',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrl: './staff-profile.component.scss'
 })
 export class StaffProfileComponent {
+  item_id:any;
+  pageLoading:any;
+  employeeDetail:any;
+
+
+  constructor(private router:Router, private api:HttpServiceService){}
+  ngOnInit(){
+    this.getItemDetail()
+  }
+
+  getParamsId(){
+    const url = window.location.href;
+    console.log('url', url);
+    const segments = url.split('/');
+    this.item_id = segments[segments.length - 1];
+
+    return this.item_id;
+  }
+
+  getItemDetail(){
+    this.pageLoading= true;
+    return this.api.get('human-resources/employees/' + this.getParamsId()).subscribe(
+      res =>{
+        let response:any = res
+        this.employeeDetail = response.data
+        console.log(this.employeeDetail)
+        this.pageLoading=false;
+      }, err=>{
+        console.log(err)
+        this.pageLoading=false;
+      }
+    )
+
+  }
+
 
 }
